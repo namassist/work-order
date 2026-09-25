48fbdcd082f54c312c68344ec1655bdd7cb801e7
namassist <chairulanmm@gmail.com>
2026-09-25 15:17:49 +0800

    chore: hapus tautan Repository dan Documentation dari sidebar

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -1,21 +1,12 @@
 <script setup lang="ts">
 import { Link } from '@inertiajs/vue3';
-import {
-    BookOpen,
-    Building2,
-    FolderGit2,
-    LayoutGrid,
-    ShieldCheck,
-    Tags,
-    Users,
-} from '@lucide/vue';
+import { Building2, LayoutGrid, ShieldCheck, Tags, Users } from '@lucide/vue';
 import { computed } from 'vue';
 import DepartmentController from '@/actions/App/Http/Controllers/Admin/DepartmentController';
 import RoleController from '@/actions/App/Http/Controllers/Admin/RoleController';
 import UserController from '@/actions/App/Http/Controllers/Admin/UserController';
 import WorkOrderCategoryController from '@/actions/App/Http/Controllers/Admin/WorkOrderCategoryController';
 import AppLogo from '@/components/AppLogo.vue';
-import NavFooter from '@/components/NavFooter.vue';
 import NavMain from '@/components/NavMain.vue';
 import NavUser from '@/components/NavUser.vue';
 import {
@@ -71,19 +62,6 @@ const can = useCan();
 const visibleAdminNavItems = computed(() =>
     visibleNavItems(adminNavItems, can),
 );
-
-const footerNavItems: NavItem[] = [
-    {
-        title: 'Repository',
-        href: 'https://github.com/laravel/vue-starter-kit',
-        icon: FolderGit2,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#vue',
-        icon: BookOpen,
-    },
-];
 </script>
 
 <template>
@@ -110,7 +88,6 @@ const footerNavItems: NavItem[] = [
         </SidebarContent>
 
         <SidebarFooter>
-            <NavFooter :items="footerNavItems" />
             <NavUser />
         </SidebarFooter>
     </Sidebar>
